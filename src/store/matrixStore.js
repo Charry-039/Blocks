b45339d7911b10da71e05e66da5cc7a314634b50
Charry <huangzubo233@gmail.com>
2024-10-24 22:18:50 +0800

    [fix]修复初始化矩阵元素重复问题

--- src/store/matrixStore.js
+++ src/store/matrixStore.js
@@ -17,15 +17,33 @@ export const useMatrix = defineStore('useMatrix', {
                 [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
                 [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
             ]),
-            isInitiated: false,
         }
     ), getters: {}, actions: {
         fullMatrix: function () {
 // 初始化随机填充矩阵
+            let dy = [-1, 0, 1, 0]
+            let dx = [0, 1, 0, -1]
             for (let i = 0; i < this.matrix.length; i++) {
                 for (let j = 0; j < this.matrix[0].length; j++) {
-                    if (this.matrix[i][j] !== -1)
+                    // 边界不进行初始化赋值
+                    if (this.matrix[i][j] === -1) {
+                        continue
+                    }
+                    this.matrix[i][j] = getRandomNumberInclusive(1, 18)
+                    // 相同元素计数器
+                    let cnt
+                    do {
+                        cnt = 0;
+                        for (let k = 0; k < 4; k++) {
+                            let ry = i + dy[k]
+                            let rx = j + dx[k]
+                            if (this.matrix[ry][rx] === this.matrix[i][j]) {
+                                ++cnt
+                            }
+                        }
+                        // 重随元素
                         this.matrix[i][j] = getRandomNumberInclusive(1, 18)
+                    } while (cnt > 1)
                 }
             }
         }
